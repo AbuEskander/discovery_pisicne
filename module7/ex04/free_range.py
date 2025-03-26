6a5aa29e967edc60162774a8180f419b42fe35a5
Proton <basselawni1@gmail.com>
2025-03-26 16:56:16 +0300

    Hayder

--- module7/ex04/free_range.py
+++ module7/ex04/free_range.py
@@ -7,8 +7,7 @@ if len(sys.argv) != 3:
     exit(1)
 
 try:
-    first_val = int(sys.argv[1])
-    second_val = int(sys.argv[2])
+    first_val, second_val = int(sys.argv[1]), int(sys.argv[2])
 except ValueError:
     print("none")
     exit(1)
